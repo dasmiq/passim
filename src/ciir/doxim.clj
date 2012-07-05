a228198db2e8ffeb6ed0629c37eb6ba41847a7f9
David Smith <dasmith@cs.umass.edu>
2012-07-05 14:49:19 -0400

    Added clustering code.

--- src/ciir/doxim.clj
+++ src/ciir/doxim.clj
@@ -1,5 +1,6 @@
 (ns ciir.doxim
   (:require [clojure.string :as s]
+            [clojure.set :as set]
             [clojure.java.shell :as sh]
             [clojure.java.io :as jio])
   (:use [clojure.math.combinatorics])
@@ -241,13 +242,144 @@
   [namef seriesf]
   (let [namei (DiskIndex/openIndexPart namef)
         smeta (load-series-meta seriesf)]
-    (doseq [line (line-seq (-> System/in java.io.InputStreamReader. java.io.BufferedReader.))]
+    (doseq [line (-> System/in java.io.InputStreamReader. java.io.BufferedReader. line-seq)]
       (println (score-pair line namei smeta)))))
+
+(defn- vocab-set
+  [s]
+  (-> s (s/split #" ") set (disj "." "###")))
+
+(defn jaccard
+  [set1 set2]
+  (/ (count (set/intersection set1 set2)) (count (set/union set1 set2))))
+
+(defn cluster-matches
+  [m thresh voc id]
+  (let [members (get-in m [:members id])]
+    (= (count members)
+       (count
+        (filter
+         #(> (jaccard voc (:vocabulary %)) thresh)
+         members)))))
+
+(defn format-cluster
+  [cluster]
+  (s/join
+   "\n"
+   (sort 
+    (loop [seen {}
+           res []
+           members cluster]
+      (if-let [cur (first members)]
+        (let [id (:id cur)]
+          (recur (assoc seen id true)
+                 (if (seen id)
+                   res
+                   (conj res
+                         (s/join "\t" ((juxt :date :title :id :url :text) cur))))
+                 (next members)))
+        res)))))
+
+(defn complete-link-reducer
+  [m line]
+  (let [[score date1 title1 url1 date2 title2 url2 sid1 sid2 series1 series2 text1 text2]
+        (s/split line #"\t")
+        id1 (Integer/parseInt sid1)
+        id2 (Integer/parseInt sid2)
+        v1 (vocab-set text1)
+        v2 (vocab-set text2)
+        rec1 {:id id1 :text text1 :vocabulary v1 :date date1 :title title1 :url url1 :score score}
+        rec2 {:id id2 :text text2 :vocabulary v2 :date date2 :title title2 :url url2 :score score}
+        nextid (inc (get m :top 0))
+        clusters1 (get-in m [:clusters id1] #{})
+        clusters2 (get-in m [:clusters id2] #{})
+        clusters (set/union clusters1 clusters2)
+        matches (when clusters
+                  (set/select
+                   (partial cluster-matches m 0.8 v1)
+                   (set/select (partial cluster-matches m 0.8 v2) clusters)))
+        match (or (first matches) nextid)]
+    ;; (println nextid)
+    ;; Actually, we should merge clusters if there is more than one match
+    (assoc
+        (if (> (count matches) 1)
+          (let [others (rest matches)
+                orecs (map (partial get (:members m)) others)
+                newrec (set/union (reduce set/union (get-in m [:members match]) orecs)
+                                  #{rec1 rec2})
+                docs (->> newrec (map :id) set seq)
+                newidx
+                (into
+                 {} (map
+                     vector docs
+                     (map #(conj (apply disj (get (:clusters m) % #{}) others) match) docs)))]
+            ;; (println match "\t" others)
+            ;; (println id1 "clusters1:" clusters1)
+            ;; (println id2 "clusters2:" clusters2)
+            ;; (println "docs:" docs)
+            ;; (println "newidx:" newidx)
+            ;; Need to dissociate the old cluster numbers from
+            ;; *all* documents, not just id1 and id2
+            ;; To test, take the first 877
+            (-> m
+                (assoc-in [:members match] newrec)
+                (assoc :clusters (merge (:clusters m) newidx))))
+          (-> m
+              (assoc-in [:members match] (set/union (get-in m [:members match])
+                                                    #{rec1 rec2}))
+              (assoc-in [:clusters id1] (conj clusters1 match))
+              (assoc-in [:clusters id2] (conj clusters2 match))))
+      :top nextid)))
+
+(defn single-link-reducer
+  [m line]
+  (let [[score date1 title1 url1 date2 title2 url2 sid1 sid2 series1 series2 text1 text2]
+        (s/split line #"\t")
+        id1 (Integer/parseInt sid1)
+        id2 (Integer/parseInt sid2)
+        cluster1 (get-in m [:cluster id1])
+        cluster2 (get-in m [:cluster id2])]
+    (cond
+     (and cluster1 cluster2) (reduce
+                              #(assoc-in %1 [:cluster %2] cluster1)
+                              (assoc-in m [:members cluster1] (set/union (get-in m [:members cluster1])
+                                                                         (get-in m [:members cluster2])))
+                              (get-in m [:members cluster2]))
+     cluster1 (-> m
+                  (assoc-in [:members cluster1] (conj (get-in m [:members cluster1]) id2))
+                  (assoc-in [:cluster id2] cluster1)
+                  (assoc-in [:text id2] text2))
+     cluster2 (-> m
+                  (assoc-in [:members cluster2] (conj (get-in m [:members cluster2]) id1))
+                  (assoc-in [:cluster id1] cluster2)
+                  (assoc-in [:text id1] text1))
+     :else (-> m
+               (assoc-in [:members id1] #{id1 id2})
+               (assoc-in [:cluster id1] id1)
+               (assoc-in [:cluster id2] id1)
+               (assoc-in [:text id1] text1)
+               (assoc-in [:text id2] text2)))))
+
+(defn cluster-scores
+  [lines]
+  (doseq [cluster
+          (->> lines
+               (reduce complete-link-reducer {})
+               :members
+               vals)]
+    (println (format-cluster cluster))))
+
+     ;; (s/join
+     ;;  "\n"
+     ;;  (sort (map #(s/join "\t" ((juxt :date :title :score :id :url :text) %)) cluster)))
+     ;; "\n")))
 
 (defn -main
   "I don't do a whole lot."
   [& args]
   (condp = (first args)
+    "cluster" (cluster-scores
+               (-> System/in java.io.InputStreamReader. java.io.BufferedReader. line-seq))
     "scores" (dump-scores (second args) (nth args 2))
     "pairs" (dump-pairs (second args) (nth args 2) (nth args 3) (nth args 4)
                         (Integer/parseInt (nth args 5)) (Integer/parseInt (nth args 6))
