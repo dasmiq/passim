97dcf81320baf660634df64d2c995d4891adc7bf
David Smith <dasmiq@gmail.com>
2016-10-04 09:04:51 -0400

    Use window functions to match boilerplate.

--- src/main/scala/PassimApp.scala
+++ src/main/scala/PassimApp.scala
@@ -5,6 +5,7 @@ import org.apache.spark.SparkContext._
 import org.apache.spark.graphx._
 import org.apache.spark.sql.{SQLContext, DataFrame, Row}
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.expressions.Window
 import org.apache.spark.storage.StorageLevel
 
 import org.apache.hadoop.fs.{FileSystem,Path}
@@ -417,46 +418,44 @@ object BoilerApp {
     val indexer = udf {(terms: Seq[String]) => hapaxIndex(config.n, terms)}
     val matchMatrix = jaligner.matrix.MatrixGenerator.generate(2, -1)
 
+    val windowMatches = udf {
+      (id: String, issue: String, index: Map[String, Int], text: String,
+        pid: Seq[String], pissue: Seq[String],
+        pindex: Seq[Map[String, Int]], ptext: Seq[String]) =>
+      val res = new ListBuffer[PassAlign]
+      for ( i <- 0 until pid.size; if pissue(i) < issue ) {
+        val inc = PassFun.increasingMatches(pindex(i)
+          .flatMap { z => if (index.contains(z._1)) Some((z._2, index(z._1), 1)) else None })
+        val gapped = PassFun.gappedMatches(config.n, config.gap, config.minAlg, inc)
+        // println("# rep: " + (pid, id, inc.size, gapped.size))
+        if ( inc.size >= config.minRep && gapped.size > 0 ) {
+          // TODO: Give high cost to newline mismatches.
+          val pt = ptext(i)
+          res += PassFun.alignStrings(config.n * 5, config.gap * 5, matchMatrix,
+            (pid(i), 0, pt.size, pt.size, pt), (id, 0, text.size, text.size, text))
+        // PassAlign(pid, id, "", "", 0, 1, 1, 0, 1, 1, 1, -1)
+        }
+      }
+      res.toSeq
+    }
+
     val raw = sqlContext.read.format(config.inputFormat).load(config.inputPaths)
     val corpus = raw.tokenize(config.text)
       .select('id, 'series, datediff('date, lit("1970-01-01")) as "day",
         'issue, indexer('terms) as "index", cleanXML('text) as "text")
-      .withColumn("daybin", ($"day" / config.history).cast("int"))
+      .na.drop(Seq("day"))
+      .sort('series, 'day)
 
-    val corpus2 = corpus.select($"id" as "pid", $"series" as "pseries",
-      $"day" as "pday", $"daybin" as "pdaybin",
-      $"issue" as "pissue", $"index" as "pindex", $"text" as "ptext")
+    // We may need to partition by year or month as well until range
+    // windows before more efficient.
+    val w = Window.partitionBy("series").orderBy("day").rangeBetween(-config.history, 0)
 
-    // The predecessor is either in the same history-sized day bin or
-    // in the previous one.  The bins are disjoint, so we don't need
-    // to dedup the result.
-    corpus2
-      .withColumn("pdaybin", $"pdaybin" + 1)
-      .unionAll(corpus2)
-      .join(corpus,
-        ($"pseries" === $"series") && ($"pdaybin" === $"daybin")
-          && ($"pissue" < $"issue") && (($"pday" + config.history) >= $"day"))
-      .select('pid, 'pindex, 'ptext, 'id, 'index, 'text)
-      .flatMap((c: Row) => c match {
-        case Row(pid: String, pindex: Map[_, _], ptext: String,
-          id: String, index: Map[_, _], text: String) => {
-          val cm = index.asInstanceOf[Map[String, Int]]
-          val pm = pindex.asInstanceOf[Map[String, Int]]
-          val inc = PassFun.increasingMatches(pm
-            .flatMap(z => if (cm.contains(z._1)) Some((z._2, cm(z._1), 1)) else None))
-          val gapped = PassFun.gappedMatches(config.n, config.gap, config.minAlg, inc)
-          // println("# rep: " + (pid, id, inc.size, gapped.size))
-          if ( inc.size >= config.minRep && gapped.size > 0 ) {
-            // TODO: Give high cost to newline mismatches.
-            Some(PassFun.alignStrings(config.n * 5, config.gap * 5, matchMatrix,
-              (pid, 0, ptext.size, ptext.size, ptext), (id, 0, text.size, text.size, text)))
-            // Some(PassAlign(pid, id, "", "", 0, 1, 1, 0, 1, 1, 1, -1))
-          } else {
-            None
-          }
-        }
-      })
-      .toDF
+    corpus
+      .select(windowMatches('id, 'issue, 'index, 'text,
+        collect_list("id").over(w), collect_list("issue").over(w),
+        collect_list("index").over(w), collect_list("text").over(w)) as "pairs")
+      .select(explode('pairs) as "pair")
+      .select($"pair.*")
       .write.json(algFname)
       // .write.parquet(algFname)
   }
