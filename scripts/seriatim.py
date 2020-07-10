Dump postings; fix bug in gap computation.

scripts/seriatim.py
@@ -2,10 +2,26 @@ import argparse
 import json, os, sys
 from math import ceil, log, inf
 from pyspark.sql import SparkSession, Row
-from pyspark.sql.functions import col, explode, size, udf, struct, length, collect_list, collect_set, sort_array, when, expr, explode, slice, map_from_entries, flatten
+from pyspark.sql.functions import col, explode, size, udf, struct, length, collect_list, collect_set, sort_array, when, expr, explode, slice, map_from_entries, flatten, xxhash64
 from pyspark.sql.types import *
 
 from dataclasses import dataclass
+
+def getPostings(text, n, floating_ngrams):
+    tf = dict()
+    posts = list()
+    for i, c in enumerate(text):
+        if c.isalnum() and ( floating_ngrams or i == 0 or not text[i-1].isalnum() ):
+            j = i + 1
+            buf = ''
+            while j < len(text) and len(buf) < n:
+                if text[j].isalnum():
+                    buf += text[j].lower()
+                j += 1
+            if len(buf) >= n:
+                tf[buf] = tf.get(buf, 0) + 1
+                posts.append((buf, i))
+    return [(key, tf[key], i) for key, i in posts]
 
 def vitSrc(pos, meta, n):
     @dataclass(frozen=True)
@@ -21,7 +37,7 @@ def vitSrc(pos, meta, n):
         score: float
 
     ## b70 docs: 21918104; 237921873725 characters
-    N = 21918104
+    N = 100 # 21918104
     pcopy = 0.8
     pcont = 0.999956
     V = 256
@@ -47,8 +63,8 @@ def vitSrc(pos, meta, n):
         # print(prev[0], file=sys.stderr)
         for m in p.alg:
             same = prev.get(m.uid, Score(0, 0, -inf))
-            gap = min(0, m.post - same.pos)
-            gap2 = min(0, p.post2 - same.pos2)
+            gap = max(0, m.post - same.pos)
+            gap2 = max(0, p.post2 - same.pos2)
             # Show we treat gaps on the source side the same? What about retrogrades?
             overlap = min(gap, gap2)
             minsub = ceil(overlap / n)
@@ -79,6 +95,8 @@ def vitSrc(pos, meta, n):
         cur = bp[cur]
     matches.reverse()
 
+    # return [(m.lab, m.pos2 - n, m.pos2, m.pos - n, m.pos) for m in matches]
+
     ## Merge matches into spans
     spans = list()
     i = 0
@@ -88,7 +106,7 @@ def vitSrc(pos, meta, n):
             j = i
             while j < len(matches):
                 if matches[j].lab == 0:
-                    if ((j+1) < len(matches) and matches[j+1].lab == matches[i].lab and
+                    if False and ((j+1) < len(matches) and matches[j+1].lab == matches[i].lab and
                         ## Allow singleton retrograde alignments
                         (matches[j+1].pos >= matches[i].pos
                          or matches[j+1].pos2 - n - matches[j].pos2 < 20)):
@@ -104,24 +122,68 @@ def vitSrc(pos, meta, n):
     return spans
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Passim Alignment')
+    parser = argparse.ArgumentParser(description='Passim Alignment',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-i', '--id', type=str, default='id',
+                        help='Field for unique document IDs')
+    parser.add_argument('-t', '--text', type=str, default='text',
+                        help='Field for document text')
+    parser.add_argument('-l', '--minDF', type=int, default=2,
+                        help='Lower limit on document frequency')
+    parser.add_argument('-u', '--maxDF', type=int, default=100,
+                        help='Upper limit on document frequency')
     parser.add_argument('-m', '--min-match', type=int, metavar='N', default=5,
                         help='Minimum number of n-gram matches between documents')
     parser.add_argument('-n', '--n', type=int, metavar='n', default=5,
                         help='n-gram order')
-    parser.add_argument('-f', '--filterpairs', type=str, default='gid < gid2',
-                        help='SQL constraint on posting pairs; default=gid < gid2')
+    parser.add_argument('--floating-ngrams',
+                        help='Allow n-grams to float from word boundaries.')
+    parser.add_argument('--fields', type=str, nargs='+', default=[],
+                        help='List of fileds to index')
+    parser.add_argument('-f', '--filterpairs', type=str, default='uid < uid2',
+                        help='SQL constraint on posting pairs; default=uid < uid2')
+    parser.add_argument('--input-format', type=str, default='json',
+                        help='Input format')
+    parser.add_argument('--output-format', type=str, default='json',
+                        help='Output format')
     parser.add_argument('inputPath', metavar='<path>', help='input data')
     parser.add_argument('outputPath', metavar='<path>', help='output')
     config = parser.parse_args()
 
     print(config)
 
+    spark = SparkSession.builder.appName('Passim Alignment').getOrCreate()
+
     dfpostFname = os.path.join(config.outputPath, 'dfpost.parquet')
     srcFname = os.path.join(config.outputPath, 'src.parquet')
 
-    spark = SparkSession.builder.appName('Passim Alignment').getOrCreate()
 
+    corpus = spark.read.option('mergeSchema',
+                               'true').format(config.input_format).load(config.inputPath
+                               ).na.drop(subset=[config.id, config.text]
+                               ).withColumn('uid', xxhash64(config.id))
+
+    termCorpus = corpus.selectExpr('uid', config.text, *config.fields)
+
+    get_postings = udf(lambda text: getPostings(text, config.n, config.floating_ngrams),
+                       ArrayType(StructType([
+                           StructField('feat', StringType()),
+                           StructField('tf', IntegerType()),
+                           StructField('post', IntegerType())])))
+
+    posts = termCorpus.withColumn('post', explode(get_postings(config.text))
+                     ).select(*[col(f) for f in termCorpus.columns], col('post.*')
+                     ).drop(config.text
+                     ).withColumn('feat', xxhash64('feat')
+                     ).filter(col('tf') == 1
+                     ).drop('tf')
+
+    df = posts.groupBy('feat').count().select('feat', col('count').cast('int').alias('df')
+             ).filter( (col('df') >= config.minDF) & (col('df') <= config.maxDF) )
+
+    posts.join(df, 'feat').write.mode('ignore').save(dfpostFname)
+    # exit(0)
+    
     vit_src = udf(lambda post, meta: vitSrc(post, meta, config.n),
                   ArrayType(StructType([
                       StructField('uid', LongType()),
@@ -130,11 +192,9 @@ if __name__ == '__main__':
                       StructField('begin', IntegerType()),
                       StructField('end', IntegerType())])))
 
-    raw = spark.read.load(config.inputPath).drop('feat', 'df2')
-
     dfpost = spark.read.load(dfpostFname)
 
-    spark.conf.set('spark.sql.shuffle.partitions', dfpost.rdd.getNumPartitions())
+    # spark.conf.set('spark.sql.shuffle.partitions', dfpost.rdd.getNumPartitions())
 
     raw = dfpost.join(dfpost.toDF(*[f + ('2' if f != 'feat' else '') for f in dfpost.columns]),
                       'feat'
@@ -145,6 +205,8 @@ if __name__ == '__main__':
     f1 = [f for f in docFields if not f.endswith('2')]
     f2 = [f for f in docFields if f.endswith('2')]
 
+    spark.conf.set('spark.sql.mapKeyDedupPolicy', 'LAST_WIN')
+    
     raw.groupBy(*docFields
       ).agg(collect_list(struct(*postFields)).alias('post')
       ).filter(size('post') >= config.min_match
@@ -157,6 +219,6 @@ if __name__ == '__main__':
       ).agg(sort_array(collect_list(struct('post2', 'df', 'alg'))).alias('post'),
             map_from_entries(flatten(collect_set('meta'))).alias('meta')
       ).withColumn('src', vit_src('post', 'meta')
-      ).write.parquet(srcFname)
+      ).write.json(srcFname)
 
     spark.stop()