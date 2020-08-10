a532067459b9bd245174b41efed02fb0b7a84d0f
David Smith <dasmiq@gmail.com>
2020-08-10 09:13:00 -0400

    Include page coordinate information in output.

--- scripts/seriatim.py
+++ scripts/seriatim.py
@@ -314,6 +314,31 @@ def clusterJoin(config, clusters, corpus):
                  ).withColumn(config.text,
                               col(config.text).substr(col('begin'), col('end') - col('begin')))
 
+    pageCol = 'pages'
+    if pageCol in out.columns:
+        pageFields = ', '.join([f'p.{f} as {f}'
+                                for f in out.selectExpr(f'inline({pageCol})').columns
+                                if f != 'regions'])
+        out = out.withColumn(pageCol, expr(f'''
+filter(transform({pageCol}, p -> struct({pageFields}, filter(p.regions, r -> r.start < end AND (r.start + r.length) > begin) as regions)), p -> size(p.regions) > 0)''')
+                ).withColumn(pageCol, expr(f'''
+transform({pageCol},
+          p -> struct({pageFields},
+                      array(aggregate(p.regions,
+                                      struct(p.regions[0].start as start,
+                                             p.regions[0].length as length,
+                                             struct(p.regions[0].coords.x as x,
+                                                    p.regions[0].coords.y as y,
+                                                    p.regions[0].coords.w as w,
+                                                    p.regions[0].coords.h as h) as coords),
+                                      (acc, r) -> struct(least(acc.start, r.start) as start,
+                                                         greatest(acc.start + acc.length, r.start + r.length) - least(acc.start, r.start) as length,
+                                                         struct(least(acc.coords.x, r.coords.x) as x,
+                                                                least(acc.coords.y, r.coords.y) as y,
+                                                                greatest(acc.coords.x + acc.coords.w, r.coords.x + r.coords.w) - least(acc.coords.x, r.coords.x) as w,
+                                                                greatest(acc.coords.y + acc.coords.h, r.coords.y + r.coords.h) - least(acc.coords.y, r.coords.y) as h) as coords))) as regions))
+'''))
+
     if config.output_format != 'parquet':
         out = out.sort(desc('size'), 'cluster', *[expr(f) for f in config.fields],
                        col(config.id), 'begin')
@@ -450,6 +475,7 @@ def main(config):
     spark.conf.set('spark.sql.shuffle.partitions', spark.sparkContext.defaultParallelism)
     spark.sparkContext.setCheckpointDir(os.path.join(config.outputPath, 'tmp'))
 
+    # TODO: Investigate why connected component computation still runs when output exists.
     clusterExtents(config,
                    spark.read.load(extentsFname)).write.mode('ignore').parquet(clustersFname)
 
