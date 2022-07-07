b7457ecf025ac32bd924a755e93d13225126baa2
David Smith <dasmiq@gmail.com>
2022-07-07 17:23:16 -0400

    Fix version number; install Windows scripts.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='passim',
-    version='0.3.0',
+    version='2.0.0-alpha.2',
     author='David A. Smith',
     author_email='dasmiq@gmail.com',
     description='Detecting and analyzing text reuse',
@@ -16,7 +16,7 @@ setuptools.setup(
         'pyspark>=3.0.1',
         'intervaltree'
     ],
-    scripts=['bin/seriatim', 'bin/passim'],
+    scripts=['bin/seriatim', 'bin/passim', 'bin/passim.cmd', 'bin/seriatim.cmd'],
     data_files=[('share', ['share/submit-seriatim.py'])],
     python_requires='>=3.6',
 )
