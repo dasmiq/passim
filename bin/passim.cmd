8281e0b79b7982bef194220b4bfda6d8f424549d
pverkind <peter.verkinderen@gmail.com>
2022-07-09 00:43:11 +0200

    Generalize the replacement commands
    Replace special characters <, >, |, & in all arguments (not just the built-in filterfields argument) with a placeholder
    which will be replaced with the original character in seriatim.py
    NB: unfortunately, this does not work with the equals sign; use _EQ_, _LTE_ and _GTE_ for such operators instead.

--- bin/passim.cmd
+++ bin/passim.cmd
@@ -1,2 +1,29 @@
 @echo off
-call %~dp0seriatim --all-pairs --fields "xxhash64(series) as gid" -f gidLESSTHANgid2 %*
+
+:: Replace special characters <, >, |, & in all arguments with a placeholder
+:: which will be replaced with the original character in seriatim.py
+:: NB: unfortunately, this does not work with the equals sign; use _EQ_, _LTE_ and _GTE_ instead.
+
+:: make sure variables are not expanded before the script is run:
+SETLOCAL EnableDelayedExpansion
+
+:: first, set up an empty variable that will contain all modified variables:
+set modifiedArgs=
+
+:: loop through all arguments passed to the script (%*)
+:: and replace all special characters in them
+:: finally, add each argument to the modified arguments list
+for %%i in (%*) do (
+  set arg=%%i
+  set arg=!arg:^<=_LT_!
+  set arg=!arg:^>=_GT_!
+  set arg=!arg:^|=_PIPE_!
+  set arg=!arg:^&=_AMPERSAND_!
+  set modifiedArgs=!modifiedArgs! !arg!
+  echo !arg!
+)
+::echo MODIFIED ARGS:
+::echo %modifiedArgs%
+
+echo %~dp0seriatim.cmd --all-pairs --fields "xxhash64(series) as gid" -f gid_LT_gid2 %modifiedArgs%
+call %~dp0seriatim.cmd --all-pairs --fields "xxhash64(series) as gid" -f gid_LT_gid2 %modifiedArgs%
